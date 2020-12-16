de596b0ef3417926cb6c6c89d5e73a64292d011e
nidhal baccouri <nidhalbacc@gmail.com>
2020-12-16 18:23:31 +0100

    =fixed path err

--- buildonic.js
+++ buildonic.js
@@ -38,16 +38,23 @@ class Buildonic {
         try {
           await this.execute("ionic capacitor add android");
           await this.execute("ionic capacitor copy android");
-          const localPropsPath = `cd ${process.cwd()}/android`;
-          const pathProvider = {cwd: localPropsPath};
+          const localPropsPath = `${process.cwd()}/android`;
+          const pathProvider = { cwd: localPropsPath };
           await this.execute("touch local.properties", pathProvider);
-          fs.writeFile(`${pathProvider}/local.properties`, `sdk.dir = ${sdkPath}`, async () => {
-            console.log("sdk path added successfully");
-            await this.execute("./gradlew assembleDebug && cd ..", pathProvider);
-            console.info(
-              `Build successfully! You can find your .apk file in ${this.platform}/app/build/outputs/debug/app-${this.mode}.apk`
-            );
-          });
+          fs.writeFile(
+            `${pathProvider}/local.properties`,
+            `sdk.dir = ${sdkPath}`,
+            async () => {
+              console.log("sdk path added successfully");
+              await this.execute(
+                "./gradlew assembleDebug && cd ..",
+                pathProvider
+              );
+              console.info(
+                `Build successfully! You can find your .apk file in ${this.platform}/app/build/outputs/debug/app-${this.mode}.apk`
+              );
+            }
+          );
         } catch (err) {
           console.error(err);
         }
@@ -116,10 +123,14 @@ class Buildonic {
   }
 
   async execute(cmd, option) {
-    console.info(`********** executing ${cmd} **********`);
-    const { stdout, stderr } = await exec(cmd, option);
-    console.log(stdout);
-    console.log(stderr);
+    try {
+      console.info(`********** executing ${cmd} **********`);
+      const { stdout, stderr } = await exec(cmd, option);
+      console.log(stdout);
+      console.log(stderr);
+    } catch (err) {
+      console.log(err);
+    }
   }
 }
 
